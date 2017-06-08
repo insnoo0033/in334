0e2ff7c2d6c3686c452c5d68fc5ca3d3cda5d1ef
insnoo0033 <pashesosol@gmail.com>
2017-06-08 17:01:26 +0430

    Create sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,8 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {@mmm3342 (https://t.me/mmm3342)},--Sudo users
+    sudo_users = {392819783
+},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
